61e10227b841f9ae6403d93648eea588be17de5c
Rocio Batres <jonasgirl21@gmail.com>
2020-09-14 22:42:00 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) 
+  while (array).size > 1 
+    if block_given?
+      yield array[element]
+    else
+      "You need a block bro." 
+    end 
+    
+  array 
+  end   
 end
