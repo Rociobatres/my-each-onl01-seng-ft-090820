afb8c89888ee744e612183ea77dbee0998e36772
Rocio Batres <jonasgirl21@gmail.com>
2020-09-14 22:50:39 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ def my_each(array)
   while (array).size > 1 
     if block_given?
       yield array.select do |item|
-        
+        item.index
     else
       "You need a block bro." 
     end 
